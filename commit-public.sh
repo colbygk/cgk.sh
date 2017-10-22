c2a42775b57068da03693318e98b0cfcb95b4407
Colby Gutierrez-Kraybill <colbygk@media.mit.edu>
2017-10-22 14:21:00 -0400

    tersting

--- commit-public.sh
+++ commit-public.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd -
+cd public && git checkout master && git add --all && git commit -m "Publishing to cgk.sh" && git push origin master && cd -
--- submodule-setup.sh
+++ submodule-setup.sh
@@ -3,4 +3,6 @@
 git submodule init
 git config -f .gitmodules.public.brach master
 git submodule update --remote
+cd public
+git pull origin master
 
